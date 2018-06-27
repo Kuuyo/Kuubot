ec29d27b307ad627e4acb49c92af391ddfb44111
Elias Hernes <brokenkyuu@gmail.com>
2018-06-28 01:05:56 +0200

    Oh yeah

--- main.rb
+++ main.rb
@@ -934,7 +934,7 @@ end
 def TimerEvent(channel,message,remainingTime,event)
 	channels = event.server.channels
 	kuupingChannel = channels.detect{|c| c.id == channel}
-	history = kuupingChannel.history(50)
+	history = kuupingChannel.history(5)
 	roles = event.server.roles
 	count = history.count{|hMessage| hMessage.content.include? message}
 	case count
