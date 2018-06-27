55679e81692dbaee1d96185214ba2e5c1dafd1cc
Elias Hernes <brokenkyuu@gmail.com>
2018-06-28 01:05:31 +0200

    Testing with fire

--- main.rb
+++ main.rb
@@ -948,6 +948,7 @@ bot.message() do |event|
 	server = event.server
 	serverId = event.server.id
 	time = Time.now
+	time = Time.utc(time.year,time.month,time.day,22,55,33)
 
 	timeCondenser1Start = Time.utc(time.year,time.month,time.day,3,0,0)
 	timeCondenser1End = Time.utc(time.year,time.month,time.day,6,0,0)
