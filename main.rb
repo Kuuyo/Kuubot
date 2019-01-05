7de243bec8a8299277cac4799322daffbb1eb8c8
Elias Hernes <brokenkyuu@gmail.com>
2019-01-05 16:09:32 +0100

    Test

--- main.rb
+++ main.rb
@@ -993,6 +993,59 @@ bot.command :dance, description:"Dance", usage:"!dance" do |event|
 	event.respond "No."
 end
 
+bot.command :connect, description:"Connects Kuubot to the voice channel", usage:"!connect" do |event|
+	case event.server.id
+		when 203172685449134080
+			bot.voice_connect(event.user.voice_channel)
+	
+		when 308345436760965120
+
+	end
+end
+
+play_desc = 'Downloads and plays a youtube video'
+stop_desc = 'Stops the currently playing music'
+yt_usage = "!play <youtube url> ` or a search ` !play <what to search for>"
+
+BASE_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&type=video&fields=items%2Fid&key=#{ENV['YOUTUBEKEY']}"
+
+bot.command([:play, :youtube, :music], usage: yt_usage, description: play_desc, min_args: 1) do |event, *songlink|
+	case event.server.id
+		when 203172685449134080
+			  unless songlink =~ /((http:[s]?\/\/)+youtube\.com|youtu.be)/i
+				  query = URI.encode(songlink.join(' '))
+				  apijson = open(BASE_URL + "&q=#{query}")
+				  response = JSON.parse(apijson.read)
+				  event.respond 'Nothing found :scream:' if response.empty?
+				  break if response.empty?
+				  songlink = 'https://www.youtube.com/watch?v=' + response['items'][0]['id']['videoId'].to_s
+			  end
+			  unless event.voice.nil?
+				  event.respond 'Already playing music'
+				  break
+			  end
+			  channel = event.user.voice_channel
+			  unless channel.nil?
+				  voice_bot = event.bot.voice_connect(channel)
+				  event.send_temp('Downloading...', 7)
+				  system("youtube-dl --no-playlist --max-filesize 50m -o 'music/#{event.server.id}.%(ext)s' -x --audio-format mp3 #{songlink}")
+				  event.respond 'Playing'
+				  voice_bot.play_file("./music/#{event.server.id}.mp3")
+				  voice_bot.destroy
+				  break
+			  end
+			  event.respond "You're not in any voice channel!"
+	
+		when 308345436760965120
+
+	end
+end
+
+bot.command(:stop, description: stop_desc) do |event|
+	  event.voice.stop_playing
+	  return nil
+end
+
 bot.command :time, description:"Displays time (standard is UTC)" do |event,target|
 	time = Time.now
 	if target != nil
